698e058e725c07304c89c2b7a2e2ba79ac3db386
luiz-spurio <luiz.spurio@gmail.com>
2020-04-06 20:31:09 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,18 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=31):
+    def __init__(self, *filhos, nome = None, idade=31):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Spurio')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Luiz'
-    print(p.nome)
-    print(p.idade)
+    luiz = Pessoa(nome='Luiz')
+    spurio = Pessoa(luiz, nome='Spurio')
+    print(Pessoa.cumprimentar(spurio))
+    print(id(spurio))
+    print(spurio.cumprimentar())
+    print(spurio.nome)
+    for filho in spurio.filhos:
+        print(filho.nome)
